69e0ffc7af1fd3430655e11b6c8718661e9c57ff
skraj_novice <95043790+skrajup@users.noreply.github.com>
2025-05-02 01:11:37 +0530

    add paint house problem

---
+++ dp/PaintHouse/brute_recursion.py
@@ -0,0 +1,18 @@
+# time: 3^N
+class Solution:
+    def distinctColoring (self, N, r, g, b):
+        # code here 
+        colors = [r,g,b]
+        # pc = prev color
+        def dfs(i, pc):
+            if i == N:
+                return 0
+            
+            min_cost = float('inf')
+            for color in range(3):
+                if color != pc:
+                    min_cost = min(min_cost, colors[color][i] + dfs(i+1,color))
+            
+            return min_cost 
+        
+        return dfs(0, -1)
---
+++ dp/PaintHouse/op1_recursion_memo.py
@@ -0,0 +1,23 @@
+# time: 3*N
+class Solution:
+    def distinctColoring (self, N, r, g, b):
+        # code here 
+        colors = [r,g,b]
+        # pc = prev color
+        dp = {}
+        def dfs(i, pc):
+            if i == N:
+                return 0
+            
+            if (i,pc) in dp:
+                return dp[(i,pc)]
+            
+            min_cost = float('inf')
+            for color in range(3):
+                if color != pc:
+                    min_cost = min(min_cost, colors[color][i] + dfs(i+1,color))
+            
+            dp[(i,pc)] = min_cost
+            return dp[(i,pc)]   
+        
+        return dfs(0, -1)
